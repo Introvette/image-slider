a3ff6839e1bd28b67f261b0f114eb1786e1d45e6
Rosario <yvette1.rosario@cms.k12.nc.us>
2023-07-10 15:11:17 -0400

    random placeholder images + styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const arr = [];
 const props = {
-    "main" : $(".container")
+    "main" : $(".slideArea")
 }
 $(document).ready(function(){
     console.log('doc ready');
@@ -13,15 +13,25 @@ function outputImages() {
         console.log(value);
         let tempActive = index == 0 ? 'active': '';
         console.log(tempActive);
-        let html = `<div class="slide ${tempActive}"><img src='${value}'<span>Caption ${index+1}</span></div>`;
+        let html = `<div class="slide ${tempActive}"><img src='${value}'><span>Caption ${index+1}</span></div>`;
         props.main.append(html);
     })
 }
 
 function makelistImages(){
     for(let x=0;x<5;x++){
-        let temp = `https://via.placeholder.com/400x400/33ee33/22ff22?text=image${x}`;
+        let temp = `https://via.placeholder.com/400x400/${ranColor()}/${ranColor()}?text=image${x}`;
         arr.push(temp);
     }
 }
 
+function ranColor() {
+    const temp = Math.random();
+    console.log(temp);
+    const temp1 = temp.toString(16);
+    console.log(temp1);
+    const temp2 = temp1.substr(2,6);
+    console.log(temp2);
+    return temp2;
+
+}
